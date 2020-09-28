1ed2fdbda3ddfd11229c6fc376b47bcf08924f98
Romanov <rjapolov1@gmail.com>
2020-09-28 09:10:06 +0300

    keydown.enter for onSubmit

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 import { AccountService } from 'src/app/services/account-service';
 import { SignINModel } from 'src/app/services/interfaces/AccountModels';
+import { URLs } from 'src/app/services/URL';
+
 
 @Component({
   selector: 'app-signin',
   templateUrl: 'signin.component.html',
-  styleUrls: ['signin.component.scss'],
+  styleUrls: [
+    'signin.component.scss'
+  ]
 })
 export class SignInComponent {
-  constructor(private http: AccountService) {}
+  constructor(private http: AccountService, private route: Router) {
+
+  }
   model: SignINModel = { email: '', password: '' };
   errorMessage: string;
 
@@ -17,14 +23,14 @@ export class SignInComponent {
 
   onSubmit(): void {
     this.http.SignIN(this.model).subscribe(
-      (result) => {
+      result => {
         if (result) {
           const token = result.token;
           localStorage.setItem('token', token);
-          //add router
+          this.route.navigate(['']);
         }
       },
-      (error) => {
+      error => {
         this.errorMessage = error.error;
       }
     );
