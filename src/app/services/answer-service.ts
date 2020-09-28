67275119c7a00179ceb00a6e6fda91dd6bcce294
Romanov <rjapolov1@gmail.com>
2020-09-28 09:34:19 +0300

    quotemarks

--- src/app/services/answer-service.ts
+++ src/app/services/answer-service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { URLs } from "./URL";
-import { UpdateAnswerModel, CreateAnswerModel, DeleteAnswerModel } from "./interfaces/AnswerModels";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { URLs } from './URL';
+import { UpdateAnswerModel, CreateAnswerModel, DeleteAnswerModel } from './interfaces/AnswerModels';
 import { Observable } from 'rxjs';
 
 @Injectable()
--- src/app/services/guards/welcome-guard.ts
+++ src/app/services/guards/welcome-guard.ts
@@ -1,8 +1,8 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
-import { Observable } from "rxjs";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
 export class WelcomeGuard implements CanActivate {
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
-        return confirm("Вы уверены, что хотите перейти?");
+        return confirm('Вы уверены, что хотите перейти?');
     }
 }
